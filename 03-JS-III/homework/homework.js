acd8301ad59d36fd93bac189b1031bd454370c3d
gisell24 <gisellmata2404@gmail.com>
2022-10-17 16:35:25 -0300

    Listo JS-III

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -5,42 +5,65 @@
 // No cambies los nombres de las funciones.
 
 function imprimirSumaNumeros() {
-    // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
-    // Tu código:
+  // Imprime la suma de los números del 1 al 10
+  // Pista: usa un acumulador
+  // Tu código:
+  var acumulador = 0;
+  for (var i = 1; i <= 10; i++) {
+    acumulador = acumulador + i;
+  }
+  return acumulador;
 }
 
-function encuentraPares(array){
+function encuentraPares(array) {
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var resultado = [];
+  var indice = 0;
+  while (indice < array.length) {
+    if (array[indice] % 2 === 0) {
+      resultado.push(array[indice]);
+    }
+    indice++;
+  }
+  return resultado;
 }
 
-function elevaAlCuadrado(array){
+function elevaAlCuadrado(array) {
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function (num) {
+    return num ** 2;
+  });
 }
 
-function sumaArray(array){
+function sumaArray(array) {
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  let suma = 0;
+  for (let i = 0; i < array.length; i++) {
+    suma += array[i];
+  }
+  return suma;
 }
 
-function numeroDigitos(num){
+function numeroDigitos(num) {
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var numeroAString = num + "";
+  return numeroAString.length;
 }
-  
-  
-  // No modificar nada debajo de esta línea
-  // --------------------------------
-  
-  module.exports = {
-    imprimirSumaNumeros,
-    encuentraPares,
-    elevaAlCuadrado,
-    sumaArray,
-    numeroDigitos
-  };
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  imprimirSumaNumeros,
+  encuentraPares,
+  elevaAlCuadrado,
+  sumaArray,
+  numeroDigitos,
+};
